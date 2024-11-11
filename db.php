a78b0860a0423e57412ae6babf2cf12506f4bb22
Vuhuuduy <duy3atv@gmail.com>
2024-11-11 14:21:53 +0700

    upload

---
+++ db.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Mở kết nối đến CSDL sử dụng PDO
+ */
+
+
+// Định nghĩa ra các tình huống lấy dữ liệu
+define('FETCH_ALL', 2);
+define('FETCH_ONE', 1);
+define('NOT_FETCH', 0);
+
+function pdo_get_connection()
+{
+    $dburl = "mysql:host=localhost;dbname=;charset=utf8";
+    $username = 'root';
+    $password = '';
+
+    $conn = new PDO($dburl, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+}
+/**
+ * Thực thi câu lệnh sql thao tác dữ liệu (INSERT, UPDATE, DELETE)
+ * @param string $sql câu lệnh sql
+ * @param array $args mảng giá trị cung cấp cho các tham số của $sql
+ * @throws PDOException lỗi thực thi câu lệnh
+ */
+function pdo_execute($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+/**
+ * Thực thi câu lệnh sql truy vấn dữ liệu (SELECT)
+ * @param string $sql câu lệnh sql
+ * @param array $args mảng giá trị cung cấp cho các tham số của $sql
+ * @return array mảng các bản ghi
+ * @throws PDOException lỗi thực thi câu lệnh
+ */
+
+
+function getData($sql, $fetchType)
+{
+    $conn = pdo_get_connection();
+    $statement = $conn->prepare($sql);
+    $statement->execute();
+
+    // Cần biết bên kia muốn lấy dl theo kiểu gì : $fetchType
+    switch ($fetchType) {
+        case FETCH_ALL:
+            return $statement->fetchAll();
+        case FETCH_ONE:
+            return $statement->fetch();
+        default:
+            return true;
+    }
+}
